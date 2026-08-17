681cc3713101b9871bea3783db7b64d5ef16d990
JaMinsane <james.jh03@gmail.com>
2026-08-16 19:09:53 -0500

    fix(ios): initialize surface props in BottomSheetSurfaceComponentView

--- ios/BottomSheetSurfaceComponentView.mm
+++ ios/BottomSheetSurfaceComponentView.mm
@@ -3,10 +3,20 @@
 #import "../common/cpp/react/renderer/components/ReactNativeBottomSheetSpec/ComponentDescriptors.h"
 
 #import <React/RCTFabricComponentsPlugins.h>
+#import <react/renderer/components/ReactNativeBottomSheetSpec/Props.h>
 
 using namespace facebook::react;
 
 @implementation BottomSheetSurfaceComponentView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+  if (self = [super initWithFrame:frame]) {
+    static const auto defaultProps = std::make_shared<const BottomSheetSurfaceViewProps>();
+    _props = defaultProps;
+  }
+  return self;
+}
 
 + (ComponentDescriptorProvider)componentDescriptorProvider
 {
